c2567b76169cf43cca54198ca8b3a9e1efc16d56
Xblester <azim.faisal23@gmail.com>
2022-11-28 21:41:21 +0800

    uploading the website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
+import Home from './components/pages/Home'
+import Services from './components/pages/Services';
+import Product from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+function App() { 
+  return ( 
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element = {<Home/>}/>
+          <Route path='/servies' element = {<Services/>}/>
+          <Route path='/products' element = {<Product/>}/>
+          <Route path='/sign-up' element = {<SignUp/>}/>
+        </Routes>
+      </Router>
+    </>
+  ); 
+}; 
 
 export default App;
